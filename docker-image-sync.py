acdd5701b99b5c838b23fa0a199d4bba14d3b5c4
peerapach varalertsakul <peerapach_v@nipa.cloud>
2018-03-29 13:42:59 +0700

    Add errors and exceptions

--- docker-image-sync.py
+++ docker-image-sync.py
@@ -3,13 +3,36 @@ import re
 import yaml
 import docker
 
-def load_config():
+global docker_client
+
+def loadConfig():
     with open('config.yml') as f:
         return yaml.load(f)
 
+def dockerLogin(registry,username,password):
+    try:
+        docker_client.login(registry=registry, 
+                            username=username,
+                            password=password) 
+    except:
+        print("Error login on" + registry)
+
+def dockerPullImage(src_repo_url):
+    try:
+        images = docker_client.images.pull(src_repo_url)
+        return(images)
+    except:
+        print("Cannot pull image from " + src_repo_url)
+
+def dockerPushImage(dst_repo_url,tag_name):
+    try:
+        docker_client.images.push(dst_repo_url,tag_name)
+    except:
+        print("Cannot pull image from " + dst_repo_url)
+
 if __name__ == '__main__':
 
-    config = load_config()
+    config = loadConfig()
 
     src_registry = config['src_registry']['hostname']
     dst_registry = config['dst_registry']['hostname']
@@ -19,26 +42,22 @@ if __name__ == '__main__':
     dst_password = None if 'password' not in config['dst_registry'] else str(config['dst_registry']['password'])
 
     docker_client = docker.from_env()
-    docker_client.login(registry=src_registry, 
-                        username=src_username,
-                        password=src_password)   
-    docker_client.login(registry=dst_registry, 
-                        username=dst_username,
-                        password=dst_password)                          
-
+    dockerLogin(src_registry,src_username,src_password)
+    dockerLogin(dst_registry,dst_username,dst_password)
+                          
     repositories = config['repositories']
 
     for repository in repositories:
         src_repo_url = src_registry + "/" + repository[0]
         dst_repo_url = dst_registry + "/" + repository[1]
         
-        images = docker_client.images.pull(src_repo_url)
-
+        images = dockerPullImage(src_repo_url)
+        
         for image in images:
             for tag in image.tags:
                 tag_name = tag.split(":")
                 image.tag(dst_repo_url,tag_name[1])
 
-                docker_client.images.push(dst_repo_url,tag_name[1])
-                # remove destination image
+                dockerPushImage(dst_repo_url,tag_name[1])
+                # remove destination image on sync host
                 docker_client.images.remove(dst_repo_url + ":" + tag_name[1])
